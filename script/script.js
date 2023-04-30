04b76c0aef4cf1c175f84abc59e6662a6bf6a9bc
Elena Sinelnikova <elle.sinelle@gmail.com>
2023-04-30 19:52:30 +0300

    feat(minimal scope): create capsLock cnd shift functionality

--- script/script.js
+++ script/script.js
@@ -1,4 +1,6 @@
 "use strict"
+let capsLock = false;
+let shift = false;
 
 let h1 = document.createElement("h1");
 h1.textContent = "RSS Виртуальная клавиатура";
@@ -28,13 +30,14 @@ keysContainer.append(createKeyboard());
 document.body.append(p);
 
 let keys = keysContainer.querySelectorAll(".keyboard__key");
-let capsLock = false;
+//let shiftBtns = querySelectorAll(".shift-btn");
+
 
 function createKeyboard() {
     let fragment = document.createDocumentFragment();
     const keyLayout = [
         "`", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-", "=", "Backspace",
-        "Tab", "q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "{", "}", "\\",
+        "Tab", "q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "[", "]", "\\",
         "CapsLock", "a", "s", "d", "f", "g", "h", "j", "k", "l", ";", "'", "Enter",
         "Shift", "z", "x", "c", "v", "b", "n", "m", ",", ".", "/", "Shift", String.fromCodePoint(129093),
         "Ctrl", "Win", "Alt", "Space", "Alt", "Ctrl", String.fromCodePoint(129092), String.fromCodePoint(129095), String.fromCodePoint(129094),
@@ -84,7 +87,7 @@ function createKeyboard() {
                 keyElement.textContent = "\\";
 
                 keyElement.addEventListener("click", () => {
-                    keyboard.value += "\\";
+                    keyboard.value += keyElement.textContent;
                     textarea.value = keyboard.value;
                 });
 
@@ -93,32 +96,33 @@ function createKeyboard() {
             case "CapsLock":
                 keyElement.classList.add("keyboard__key-wide");
                 keyElement.textContent = "CapsLock";
-/*
+
                 keyElement.addEventListener("click", () => {
                     toggleCapsLock();
                     keyElement.classList.toggle("keyboard__key-caps-active");
                 })
-*/
+
                 break;
 
             case "Shift":
                 keyElement.classList.add("keyboard__key-wide");
+                keyElement.classList.add("shift-btn");
                 keyElement.textContent = "Shift";
-    /*
-                        keyElement.addEventListener("keydown", () => {
-                            toggleShift();
-                        });*/
+
+                keyElement.addEventListener("mousedown", () => {
+                    toggleShift()
+                });
+
+                keyElement.addEventListener("mouseup", () => {
+                    toggleShift()
+                });
 
                 break;
 
             case "Ctrl":
                 keyElement.classList.add("keyboard__key-wide");
                 keyElement.textContent = "Ctrl";
-    /*
-                        keyElement.addEventListener("click", () => {
-                            toggleCtrl();
-                        });
-    */
+
                 break;
 
             case "Enter":
@@ -145,11 +149,7 @@ function createKeyboard() {
 
                 case "Alt":
                     keyElement.textContent = "Alt";
-    /*
-                        keyElement.addEventListener("click", () => {
-                            toggleAlt();
-                        });
-    */
+
                     break;
 
                 case "Win":
@@ -173,4 +173,127 @@ function createKeyboard() {
     });
 
     return fragment;
+}
+
+function toggleCapsLock() {
+    capsLock = !capsLock;
+
+    for (let key of keys) {
+        if (key.textContent !== "Backspace"
+            && key.textContent !== "Tab"
+            && key.textContent !== "CapsLock"
+            && key.textContent !== "Enter"
+            && key.textContent !== "Shift"
+            && key.textContent !== "Ctrl"
+            && key.textContent !== "Win"
+            && key.textContent !== "Alt"
+            && key.textContent !== "Space") {
+                key.textContent = capsLock === true
+                ? key.textContent.toUpperCase()
+                : key.textContent.toLowerCase();
+        }
+    }
+}
+
+function toggleShift() {
+    shift = !shift;
+
+    for (let key of keys) {
+        if (key.textContent !== "Backspace"
+            && key.textContent !== "Tab"
+            && key.textContent !== "CapsLock"
+            && key.textContent !== "Enter"
+            && key.textContent !== "Shift"
+            && key.textContent !== "Ctrl"
+            && key.textContent !== "Win"
+            && key.textContent !== "Alt"
+            && key.textContent !== "Space") {
+                key.textContent = shift === true
+                ? key.textContent.toUpperCase()
+                : key.textContent.toLowerCase();
+        }
+
+        if (key.textContent === "`" || key.textContent === "~") {
+            key.textContent = shift === true ? "~" : "`";
+        }
+
+        if (key.textContent === "1" || key.textContent === "!") {
+            key.textContent = shift === true ? "!" : "1";
+        }
+
+        if (key.textContent === "2" || key.textContent === "@") {
+            key.textContent = shift === true ? "@" : "2";
+        }
+
+        if (key.textContent === "3" || key.textContent === "#") {
+            key.textContent = shift === true ? "#" : "3";
+        }
+
+        if (key.textContent === "4" || key.textContent === "$") {
+            key.textContent = shift === true ? "$" : "4";
+        }
+
+        if (key.textContent === "5" || key.textContent === "%") {
+            key.textContent = shift === true ? "%" : "5";
+        }
+
+        if (key.textContent === "6" || key.textContent === "^") {
+            key.textContent = shift === true ? "^" : "6";
+        }
+
+        if (key.textContent === "7" || key.textContent === "&") {
+            key.textContent = shift === true ? "&" : "7";
+        }
+
+        if (key.textContent === "8" || key.textContent === "*") {
+            key.textContent = shift === true ? "*" : "8";
+        }
+
+        if (key.textContent === "9" || key.textContent === "(") {
+            key.textContent = shift === true ? "(" : "9";
+        }
+
+        if (key.textContent === "0" || key.textContent === ")") {
+            key.textContent = shift === true ? ")" : "0";
+        }
+
+        if (key.textContent === "-" || key.textContent === "_") {
+            key.textContent = shift === true ? "_" : "-";
+        }
+
+        if (key.textContent === "=" || key.textContent === "+") {
+            key.textContent = shift === true ? "+" : "=";
+        }
+
+        if (key.textContent === "[" || key.textContent === "{") {
+            key.textContent = shift === true ? "{" : "[";
+        }
+
+        if (key.textContent === "]" || key.textContent === "}") {
+            key.textContent = shift === true ? "}" : "]";
+        }
+
+        if (key.textContent === "\\" || key.textContent === "|") {
+            key.textContent = shift === true ? "|" : "\\";
+        }
+
+        if (key.textContent === ";" || key.textContent === ":") {
+            key.textContent = shift === true ? ":" : ";";
+        }
+
+        if (key.textContent === "'" || key.textContent === "\"") {
+            key.textContent = shift === true ? "\"" : "'";
+        }
+        if (key.textContent === "," || key.textContent === "<") {
+            key.textContent = shift === true ? "<" : ",";
+        }
+
+        if (key.textContent === "." || key.textContent === ">") {
+            key.textContent = shift === true ? ">" : ".";
+        }
+
+        if (key.textContent === "?" || key.textContent === "/") {
+            key.textContent = shift === true ? "?" : "/";
+        }
+    }
 }
